20aa56ec8a42dc3ad96d1c12366d25fed316a6d0
Bryan <david_bryan34@hotmail.com>
2022-06-10 00:10:09 +0100

    Set up .env

--- src/pages/MovieListPage.tsx
+++ src/pages/MovieListPage.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+
+const api = import.meta.env.VITE_API_KEY_TMDB;
 
 export const MovieListPage = () => {
+  console.log(api);
   return (
     <div>MovieListPage</div>
   )
